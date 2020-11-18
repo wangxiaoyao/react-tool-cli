2ffefa48864c588d8d1d3c802d362fd75f024866
王逍遥 <wangxiaoyao77@gmail.com>
2020-11-18 16:25:27 +0800

    完成:拖拽

--- src/page/GridLayout_lib/index.jsx
+++ src/page/GridLayout_lib/index.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import GridLayout from "react-grid-layout";
+import styles from "./style.less";
 
-const GridLayout_lib = () => {
-  return <h1>页面拖拽</h1>;
-};
+class GridLayout_lib extends React.Component {
+  render() {
+    // layout is an array of objects, see the demo for more complete usage
+    const layout = [
+      { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 2 },
+    ];
+    return (
+      <div>
+        <h1>页面拖拽</h1>
+        <GridLayout
+          className="layout"
+          layout={layout}
+          cols={12}
+          rowHeight={30}
+          width={1200}
+        >
+          <div className={styles.box} key="a">
+            a
+          </div>
+          <div className={styles.box} key="b">
+            b
+          </div>
+          <div className={styles.box} key="c">
+            c
+          </div>
+        </GridLayout>
+      </div>
+    );
+  }
+}
 
 export default GridLayout_lib;
