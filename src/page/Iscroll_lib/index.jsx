4e8f3fa2ca3ffa7a571ab22e829b02c7fb79239f
王逍遥 <wangxiaoyao77@gmail.com>
2020-11-18 15:57:51 +0800

    完成下拉

--- src/page/Iscroll_lib/index.jsx
+++ src/page/Iscroll_lib/index.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+// import IScroll from "iscroll/build/iscroll"; // 普通版
+import IScroll from "iscroll/build/iscroll-probe"; // 复杂版
+// import IScroll from "iscroll/build/iscroll-infinite";
 
 const Iscroll_lib = () => {
-  return <h1>下拉功能</h1>;
+  useEffect(() => {
+    console.log("初始化");
+    var myScroll = new IScroll("#wrapper", {
+      mouseWheel: true,
+      scrollbars: true,
+      bounce: true,
+    });
+    myScroll.on("scrollStart", handleStartScroll);
+    myScroll.on("scrollEnd", function () {
+      if (this.y < -1) {
+        console.log("展示");
+      }
+    });
+  }, []);
+  const handleStartScroll = () => {
+    console.log("开始滚动");
+  };
+  return (
+    <div>
+      <h1>下拉功能</h1>
+      <div id="wrapper" style={{ height: "100px", overflow: "hidden" }}>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li> <li>1</li>
+          <li>2</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Iscroll_lib;
